f0babce0599926f8c213077830ef4071d0dc2b9b
alexgisa525 <82471720+alexgisa525@users.noreply.github.com>
2021-05-13 15:45:10 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,20 +2,24 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level1":return tiles.createTilemap(hex`20000800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000060000000000000800000000000000000000000000000000000000000000000003070a000000080808000000000000000000000000000000000000000000000009120500000808080808000000000000000000000000000213140000000000000912050008080808080808000000000000001500000000010104040404040401011205080e0b0f100c110d080707070707070700000000`, img`
+            case "level1":return tiles.createTilemap(hex`200008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001500000000000007000000000000000000000000000000000000000000000000030609000000070707000000000000000000000000000000000000000000000008110500000707070707000000000000001414000000000212130000000000000811050007070707070707000000000015141400000000010104040404040401011105070d0a0e0f0b100c070606060606060600000000`, img`
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 2 . . . . . . . . . . . . . . . 
 . . . . . . . . . 2 2 2 . . . 2 . 2 . . . . . . . . . . . . . . 
 . . . . . . . . . 2 . 2 . . 2 . . . 2 . . . . . . . . . . . . . 
-. . . . . . . . . 2 . 2 . 2 . . . . . 2 . . . . . . . . . . . . 
+2 2 2 . . . . . . 2 . 2 . 2 . . . . . 2 . . . . . . . . . . . . 
 2 2 2 2 2 2 2 2 2 2 . 2 2 . . . . . . . 2 2 2 2 2 2 2 2 . . . . 
-`, [myTiles.transparency16,sprites.builtin.oceanDepths0,sprites.builtin.coral1,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.darkGroundEast,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundNorthEast0,sprites.builtin.crowd1,sprites.builtin.crowd4,sprites.builtin.crowd6,sprites.builtin.crowd0,sprites.builtin.crowd2,sprites.builtin.crowd3,sprites.builtin.crowd5,sprites.builtin.brick,sprites.builtin.coral2,sprites.builtin.coral5,sprites.dungeon.chestClosed], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.builtin.oceanDepths0,sprites.builtin.coral1,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundNorthEast0,sprites.builtin.crowd1,sprites.builtin.crowd4,sprites.builtin.crowd6,sprites.builtin.crowd0,sprites.builtin.crowd2,sprites.builtin.crowd3,sprites.builtin.crowd5,sprites.builtin.brick,sprites.builtin.coral2,sprites.builtin.coral5,myTiles.tile1,sprites.dungeon.collectibleBlueCrystal], TileScale.Sixteen);
         }
         return null;
     })
@@ -23,6 +27,10 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile":
+            case "tile1":return tile1;
         }
         return null;
     })
